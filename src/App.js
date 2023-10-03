a7c3da0a13ec2e93dd905aa1fcfcf732e8e5325b
Emmanuel Vidal <edmvp98@gmail.com>
2023-10-02 22:45:21 -0600

    primeros componentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './imgs/logo.svg';
 import './styles/App.css';
+import Pokedex from './components/Pokedex';
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <section>
+          <Pokedex></Pokedex>
+        </section>
       </header>
     </div>
   );
--- src/components/Pokedex.js
+++ src/components/Pokedex.js
@@ -1,5 +1,18 @@
 import React from  'react'
 
 class Pokedex extends React.Component{
-    
+    render(){
+        return (
+            <div>
+                <h1>
+                    hola
+                </h1>
+                <h2>
+                    estas dentro de Pokedex
+                </h2>
+            </div>
+        )
+    }
 }
+
+export default Pokedex
