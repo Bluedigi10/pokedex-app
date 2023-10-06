2b999db602fd16d3300a956bdd20c5b32c64d24b
EmmanuelDMV <emartinez@ahorra.io>
2023-10-06 15:08:19 -0600

    implementando cambio de color de acuerdo al tipo

--- src/components/Type.js
+++ src/components/Type.js
@@ -1,6 +1,20 @@
 import React from "react";
 
 function Type({item}){
+    const obj = document.getElementsByClassName('pokemon-type');
+    var color;
+    switch(item.type.name){
+        case 'bug':
+            color = '#A8B820'
+            break
+        case 'water':
+            color = '#3899F8'
+            break
+        default:
+            color = '#ffffff'
+            
+    }
+    obj.style = `background-color: ${color}`
     return(
         <li className="pokemon-type">
             <span className="type-name">{item.type.name}</span>
